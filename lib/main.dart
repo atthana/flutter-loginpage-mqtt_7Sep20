86ed20aa05e302c081b0ae5ee92954214f72d38e
atthana-p <atthana.p@codium.co>
2020-09-07 20:37:57 +0700

    Updated MQTT all pages and tested work fine.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:day12_login/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'mqtt/state/MQTTAppState.dart';
+import 'pages/login.dart';
+import 'widgets/mqttview.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +27,11 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(primarySwatch: Colors.blue),
       theme: ThemeData(primaryColor: Color(0xFF6065D6)),
       title: 'Dogelbow',
-      home: LoginPage(),
+      home: ChangeNotifierProvider<MQTTAppState>(
+        create: (_) => MQTTAppState(),
+        child: MQTTView(),
+        // child: LoginPage(),
+      )
     );
   }
 }
